75c946dc10f2c44ee125e856ac2142b7ef0d8c62
dorog <dorogi.x@gmail.com>
2019-01-09 20:21:18 +0100

    Buggfix
    Movement

---
+++ Assets/Character/SideBlock.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class SideBlock : MonoBehaviour
+{
+    [SerializeField]
+    private bool isRight;
+
+    [SerializeField]
+    private CharacterMovement charMovement;
+
+    private int counter = 0;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        counter++;
+        if (counter == 1)
+        {
+            charMovement.SetBlockLaneSide(isRight, true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        counter--;
+        if (counter == 0)
+        {
+            charMovement.SetBlockLaneSide(isRight, false);
+        }
+    }
+}
