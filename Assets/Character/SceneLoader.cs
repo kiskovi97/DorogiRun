b32425264c140cbe378404a64a0c4e817e2fec00
dorog <dorogi.x@gmail.com>
2019-01-17 19:08:41 +0100

    Game scene: buggfix, ui, end option
    Buggfix: Johnny controller, bad transition
    UI: Pause menu, GameOver menu, Score
    End option: Johnny can die now

--- Assets/Character/SceneLoader.cs
+++ Assets/Character/SceneLoader.cs
@@ -1,10 +1,27 @@
-﻿using UnityEngine;
-using UnityEngine.SceneManagement;
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
 
-public class SceneLoader
+public class SceneLoader : MonoBehaviour
 {
+    [SerializeField]
+    private string LobbyScene = "Lobby";
+    [SerializeField]
+    private string GameOverScene = "GameOver";
+    [SerializeField]
+    private string GameScene = "Game";
+
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void GoToLobby()
+    {
+        SceneManager.LoadScene(LobbyScene);
+    }
+
+    public void GoToGameOverScene()
+    {
+        SceneManager.LoadScene(GameOverScene);
     }
 }
---
+++ Assets/Gameover.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Gameover : MonoBehaviour
+{
+    public delegate void ContinueGame();
+
+    public event ContinueGame continueGame;
+
+    private string playerTag = "Player";
+
+    [SerializeField]
+    private GameObject continueQuestion;
+    [SerializeField]
+    private SliderCounter sliderCounter;
+    [SerializeField]
+    private string gameOverScene = "GameOver";
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == playerTag)
+        {
+            continueQuestion.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+
+    public void Continue()
+    {
+        Time.timeScale = 1;
+        continueQuestion.SetActive(false);
+        continueGame();
+    }
+
+    public void GameOver()
+    {
+        Time.timeScale = 1;
+        SceneLoader sceneLoader = new SceneLoader();
+        sceneLoader.LoadScene(gameOverScene);
+    }
+}
