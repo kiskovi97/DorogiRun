c9a6756c5782f4e80c5a617a781af2c435f67b87
kiskovi97 <kiskovi97@gmail.com>
2019-01-20 15:57:50 +0100

    Collactables and Texture refactor

---
+++ Assets/Map/Scripts/Collactables/Coin.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : Collactable
+{
+    protected override void TriggerFunction(Collider other)
+    {
+        Debug.Log("Do Coin Stuff");
+    }
+}
---
+++ Assets/Map/Scripts/Collactables/Collactable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Collactable : MovingObject
+{
+    public void OnTriggerEnter(Collider other)
+    {
+        TriggerFunction(other);
+        Destroy(this, 0.01f);
+    }
+
+    protected abstract void TriggerFunction(Collider other);
+}
