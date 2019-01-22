d8ae4819b630be97ceaa4f31f5df78b3fe18bb88
dorog <dorogi.x@gmail.com>
2019-01-22 13:15:54 +0100

    Clean the mess, Buggfix: jump in air and faster fall option
    Clean the mess: New folders, few scripts has new place
    Buggfix: character isnt able to jump in air after fall down from something
    Faster fall option: Character falls faster if u slide

---
+++ Assets/Character/Scripts/GroundChecker.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class GroundChecker : MonoBehaviour
+{
+    private int count = 0;
+
+    [SerializeField]
+    private CharacterMovement characterMovement;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Collactable script = other.GetComponent<Collactable>();
+        if (script != null)
+        {
+            return;
+        }
+        count++;
+        if(count == 1)
+        {
+            characterMovement.SetJump(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Collactable script = other.GetComponent<Collactable>();
+        if (script != null)
+        {
+            return;
+        }
+        count--;
+        if(count == 0)
+        {
+            characterMovement.SetJump(false);
+        }
+    }
+}
