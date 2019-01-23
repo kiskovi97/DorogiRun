80ead28a7f3067f845e8cf8a3c32fcd2c5fc10e2
dorog <dorogi.x@gmail.com>
2019-01-24 00:59:49 +0100

    Count down
    Count down works after press pause's continue button

--- Assets/MenuScripts/Game/Pause.cs
+++ Assets/MenuScripts/Game/Pause.cs
@@ -1,9 +1,17 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class Pause : MonoBehaviour
 {
     [SerializeField]
     private GameObject pauseMenu;
+    [SerializeField]
+    private Text counter;
+    [SerializeField]
+    private int countMax = 3;
+
+    private int count;
 
     private float prevTime = 1f;
 
@@ -16,7 +24,40 @@ public class Pause : MonoBehaviour
 
     public void ContinueGame()
     {
-        Time.timeScale = prevTime;
+        //Time.timeScale = prevTime;
+        count = countMax;
+        CountDown();
         pauseMenu.SetActive(false);
+    }
+
+    private void CountDown()
+    {
+        if (count == countMax)
+        {
+            counter.gameObject.SetActive(true);
+        }
+        counter.text = "" + count;
+        count--;
+        if (count != -1)
+        {
+            StartCoroutine(CallInTime(1f));
+        }
+        else
+        {
+            count = countMax;
+            counter.gameObject.SetActive(false);
+            Time.timeScale = prevTime;
+        }
+    }
+
+    private IEnumerator CallInTime(float time)
+    {
+        var t = 0f;
+        while (t < 1)
+        {
+            t += Time.unscaledDeltaTime / time;
+            yield return null;
+        }
+        CountDown();
     }
 }
